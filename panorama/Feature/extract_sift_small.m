aec575e9b76b9f5284e0a01e3168cd33e558f961
Yunfeng Wang <wangyf11@mail.ustc.edu.cn>
2016-05-06 20:53:32 +0800

    add code for extracting sift, adding parameters to vlfeat function

---
+++ panorama/Feature/extract_sift_small.m
@@ -0,0 +1,26 @@
+datasetRootPath = 'D:\yunfeng\Documents\Visual Studio 2012\Projects\iamge-understanding\Panorama\pano1';
+imageIdRange = 8:25;
+imageIdCell = num2cell(imageIdRange);
+
+for i = 1: size(imageIdCell,2)
+    jpgFileName = strcat(datasetRootPath, '\jpg\pano1_00',sprintf('%02d',imageIdCell{i}), '.JPG');
+    I = imread(jpgFileName);
+    I = single(rgb2gray(I));
+    [f, d] = vl_sift(I, 'EdgeThresh', 2);
+
+    %normalize 
+  
+    d = normc(double(d));
+    [dimOfFeat,numOfFeat]  = size(d);
+    
+    %write out feature
+    siftFileName = strcat(datasetRootPath, '\sift_small\pano1_00', sprintf('%02d', imageIdCell{i}), '.f');
+    siftFileId = fopen(siftFileName, 'w');
+    fprintf(siftFileId, '%d\n', numOfFeat);
+    fprintf(siftFileId, '%d\n', dimOfFeat);
+    fprintf(siftFileId, '%f %f %f %f\n', f(1), f(2), f(3), f(4));
+    fclose(siftFileId);
+    
+    dlmwrite(siftFileName, d', '-append', 'delimiter', ' ');
+    
+end
